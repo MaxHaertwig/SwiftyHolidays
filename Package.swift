b0816ae7cf7e4b1ef538ffd9c4834b965213682b
Max Haertwig <dev@maxhaertwig.com>
2020-01-07 09:59:05 +0100

    Reformat Package.swift

--- Package.swift
+++ Package.swift
@@ -6,23 +6,11 @@ import PackageDescription
 let package = Package(
     name: "SwiftyHolidays",
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .library(
-            name: "SwiftyHolidays",
-            targets: ["SwiftyHolidays"]),
+        .library(name: "SwiftyHolidays", targets: ["SwiftyHolidays"])
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
+    dependencies: [],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
-            name: "SwiftyHolidays",
-            dependencies: []),
-        .testTarget(
-            name: "SwiftyHolidaysTests",
-            dependencies: ["SwiftyHolidays"]),
+        .target(name: "SwiftyHolidays", dependencies: []),
+        .testTarget(name: "SwiftyHolidaysTests", dependencies: ["SwiftyHolidays"])
     ]
 )
